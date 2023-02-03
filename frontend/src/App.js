d01f66c3e3759e189755447c1665e4b21625e1d9
Kevin Willian Candeloni <kcandeloni@gmail.com>
2023-02-03 20:08:51 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Lanchão..... seráçy??</h1>
+        <h1>Lanchão..... da Laura??</h1>
         <ul>
           { snacks.map((value, id) => (<li key={id}>
             {value}
